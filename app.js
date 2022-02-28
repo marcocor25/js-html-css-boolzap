88dd94093bd755f04273f2576734d5a9ba69c276
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-28 16:14:57 +0100

    Caricamento file esercizio

---
+++ app.js
@@ -0,0 +1,15 @@
+// TEST
+console.log('Welcome to the new version of WhatsApp Web!');
+
+// COLLEGAMENTO VUEJS
+const app = new Vue ({
+    el: '#root',
+    data: {
+
+    },
+    methods: {
+        
+    },
+});
+
+console.log(app);
